085e10a01579f39ebc37c3c1e27bdc36a83e8801
varunraj <varunraj82@gmail.com>
2020-04-30 09:18:40 -0400

    added add-mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
 
 class SongCreate extends Component{
 
@@ -7,11 +9,23 @@ class SongCreate extends Component{
         this.state = {title:''};
     }
 
+    onSubmit(event){
+        
+        event.preventDefault(); // prevent default behavior of form submitting by iteself.
+        // calling mutation function on props with query params will run mutation query.
+        
+        this.props.mutate({
+            variables:{
+                title:this.state.title
+            }
+        });
+    }
+
     render(){
         return(
             <div>
                 <h3>Create a new Song</h3>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input 
                         onChange = {event => this.setState({title:event.target.value})}
@@ -22,5 +36,13 @@ class SongCreate extends Component{
         );
     }
 }
+// We need to get data from react component to query. We have to use query parameters here.
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong (title:$title){
+            title
+        }
+    }
+`;
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
